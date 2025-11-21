fff39bca89252eb3911df516f50b735e3a083af6
Sarujanan <sarujanang@gmail.com>
2025-11-21 23:13:15 +0530

    fixed issues

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { changeBookingStatus, checkCarAvailabilityOfCar, createBooking, getOwnerBookings, getUserBookings } from "../controllers/bookingController";
+import { changeBookingStatus, checkCarAvailabilityOfCar, createBooking, getOwnerBookings, getUserBookings } from "../controllers/bookingController.js";
 import { protect } from "../middleware/auth.js";
 
 const bookingRouter = express.Router();
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { addCar, changeRoleToOwner } from "../controllers/ownerContoller.js";
+import { addCar, changeRoleToOwner, deleteCar, getDashboardData, getOwnerCars, toggleCarAvailability, updateUserImage } from "../controllers/ownerContoller.js";
 import upload from "../middleware/multer.js";
 
 const ownerRouter = express.Router();
